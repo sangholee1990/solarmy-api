07ae607d47f5dc97f7dd56fb5a7d46ca69c1a690
shlee1990 <sangho.lee.1990@gmail.com>
2023-07-09 22:53:13 +0900

    commit

--- src/main/java/solarmy/front/vo/TermsCondVO.java
+++ src/main/java/solarmy/front/vo/TermsCondVO.java
@@ -1,5 +1,6 @@
 package solarmy.front.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,12 +9,18 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(callSuper = false)
 public class TermsCondVO {
-	private Integer customer_link_number;	
-	private String terms_code;	
-	private String terms_version;	
-	private String terms_cont;	
-	private String agrtr_cndtn_yn;	
-	private String use_yn;	
-	private String mod_date;	
-	private String reg_date;
+    @ApiModelProperty(value = "고객 연동 번호", required = true)
+    private Integer customer_link_number;
+
+    @ApiModelProperty(value = "약관 코드", required = false)
+    private String terms_code;
+
+    @ApiModelProperty(value = "약관 버전", required = false)
+    private String terms_version;
+
+    private String terms_cont;
+    private String agrtr_cndtn_yn;
+    private String use_yn;
+    private String mod_date;
+    private String reg_date;
 }
